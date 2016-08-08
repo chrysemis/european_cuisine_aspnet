68cc943a89b19449e8f6907787a94b063415144f
chrysemis <mixerik@gmail.com>
2016-08-08 11:58:58 +0100

    Added Recipe Controller and EuropeanFood/Recipe View

---
+++ src/EuropeanFood/Controllers/EuropeanFoodController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EuropeanFood.Controllers
+{
+    public class EuropeanFoodController : Controller
+    {
+        public ActionResult Recipe()
+        {
+            return View();
+        }
+    }
+}
