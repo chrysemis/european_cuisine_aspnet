83dd60275a5136af034b7cbf15b2f6c8492f4d5f
chrysemis <mixerik@gmail.com>
2016-08-08 19:58:25 +0100

    Added recipe model

---
+++ src/EuropeanFood/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuropeanFood.Models
+{
+    public class Recipe
+    {
+        public string RecipeName { get; set; }
+        public string Description { get; set; }
+        public string RecipeImageUrl { get; set;  }
+        public string[] Ingredients { get; set; }
+        public string[] Method { get; set;  }
+    }
+}
