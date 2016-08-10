683f4cc8c1042c30f6398003dfc42c0359573872
chrysemis <mixerik@gmail.com>
2016-08-10 10:29:22 +0100

    Updated the default route

--- src/EuropeanFood/App_Start/RouteConfig.cs
+++ src/EuropeanFood/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EuropeanFood
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "EuropeanFood", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
